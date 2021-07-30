5c3c0d19d86fbeeec5e63cada60ebc0a93f95036
Waranya T <riicne@gmail.com>
2021-07-30 16:09:17 +0700

    Add files via upload
    แสดงภาพไฟล์ .GIF โดยใช้ QMovie

---
+++ animation.py
@@ -0,0 +1,28 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtCore import Qt, QTimer 
+from PyQt5.QtGui import QMovie
+
+class EyesScreen(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setFixedSize(1080,800)
+        
+        label_animation = QLabel(self)
+
+        self.movie = QMovie('eyes.gif')
+        label_animation.setMovie(self.movie)
+
+        self.startAnimation()
+
+        self.show()
+
+    def startAnimation(self):
+        self.movie.start()
+
+app = QApplication(sys.argv)
+
+demo = EyesScreen()
+demo.show()
+
+app.exit(app.exec_())
